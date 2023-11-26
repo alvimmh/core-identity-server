4bc9b57d1aa0eb3ea11e3c7421dcc7f95a2f70ef
Md Mahidul Haque Alvi <mahidulalvi@proton.me>
2023-11-25 23:04:17 -0600

    Updated client model after Duende Identity Server version update

--- src/CoreIdentityServer/Internals/Migrations/Auxiliary/ConfigurationDb/20231126045232_UpdateClientModel.Designer.cs
+++ src/CoreIdentityServer/Internals/Migrations/Auxiliary/ConfigurationDb/20231126045232_UpdateClientModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Duende.IdentityServer.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace CoreIdentityServer.Internals.Migrations.Auxiliary.ConfigurationDb
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    partial class ConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231126045232_UpdateClientModel")]
+    partial class UpdateClientModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/CoreIdentityServer/Internals/Migrations/Auxiliary/ConfigurationDb/20231126045232_UpdateClientModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreIdentityServer.Internals.Migrations.Auxiliary.ConfigurationDb
+{
+    /// <inheritdoc />
+    public partial class UpdateClientModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "CoordinateLifetimeWithUserSession",
+                table: "Clients",
+                type: "boolean",
+                nullable: true);
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "DPoPClockSkew",
+                table: "Clients",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<int>(
+                name: "DPoPValidationMode",
+                table: "Clients",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InitiateLoginUri",
+                table: "Clients",
+                type: "character varying(2000)",
+                maxLength: 2000,
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "RequireDPoP",
+                table: "Clients",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CoordinateLifetimeWithUserSession",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "DPoPClockSkew",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "DPoPValidationMode",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "InitiateLoginUri",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "RequireDPoP",
+                table: "Clients");
+        }
+    }
+}
